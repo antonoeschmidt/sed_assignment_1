dcef03f33b59b172816c4b195056e875b07e1a6f
Jeppe Ekstrand Halkjaer Madsen <jehm@sund.ku.dk>
2021-04-11 12:23:30 +0700

    remove residuals and do it the old fashioned way

--- test.cpp
+++ test.cpp
@@ -2,32 +2,52 @@
 #include <fstream>
 #include <string>
 #include <math.h>
+#include <iomanip>
 
 using namespace std;
 
-void swap(double* a, double* b) {
-	double t = *a;
-	*a = *b;
-	*b = t;
+
+void groupinfo()
+{
+    cout << "ASSIGNMENT 1 GROUP 30" << endl;
+    cout << "s3891411, s3891411@rmit.edu.vn, Jeppe Ekstrand Halkjaer Madsen" << endl;
+    cout << "s3877375, s3877375@rmit.edu.vn, Le Pham Ngoc Hieu" << endl;
+    cout << "s3775271, s3775271@rmit.edu.vn, Ramanaharan Ramcharan" << endl;
+    cout << "s3892062, s3892062@rmit.edu.vn, Anton Ostergaard Schmidt" << endl;
 }
 
+void formatoutput(double output)
+{
+    cout << setprecision(5) << output;
+}
+
+// *** QuickSort from Teacher *** //
+void swap(double *a, double *b)
+{
+    double t = *a;
+    *a = *b;
+    *b = t;
+}
 /* This function takes last element as pivot, places
  * the pivot element at its correct position in sorted
  * array, and places all smaller (smaller than pivot)
  * to left of pivot and all greater elements to right
  * of pivot */
-int partition (double arr[], int low, int high) {
-	int pivot = arr[high];
-	int i = (low - 1);
+int partition(double arr[], int low, int high)
+{
+    double pivot = arr[high];
+    int i = (low - 1);
 
-	for (int j = low; j <= high- 1; j++) {
-		if (arr[j] <= pivot) {
-			i++;
-			swap(&arr[i], &arr[j]);
-		}
-	}
-	swap(&arr[i + 1], &arr[high]);
-	return (i + 1);
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
 }
 
 /* The main function that implements QuickSort
@@ -35,25 +55,30 @@ int partition (double arr[], int low, int high) {
  * low --> Starting index,
  * high --> Ending index
  */
-void quickSort(double arr[], int low, int high) {
-	if (low < high) {
-		/* pi is partitioning index, arr[p] is now
+void quickSort(double arr[], int low, int high)
+{
+    if (low < high)
+    {
+        /* pi is partitioning index, arr[p] is now
 		at right place */
-		int pi = partition(arr, low, high);
+        int pi = partition(arr, low, high);
 
-		// Separately sort elements before
-		// partition and after partition
-		quickSort(arr, low, pi - 1);
-		quickSort(arr, pi + 1, high);
-	}
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
 }
 
-void print_array(int arr[], int size) {
-	int i;
-	for (i=0; i < size; i++)
-		cout << arr[i] <<  " ";
-	cout << endl;
+void print_array(double arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
 }
+
+// *** END QUICKSORT ** //
 
 /* Splitting the string x,y into integers x and y by finding ',' and using it as a 
 reference to split the string*/
@@ -71,27 +96,53 @@ void splitting(string buffer, string delimiter, double *x, double *y)
     *y = stof(buffer.substr(startpos, endpos - startpos));
 }
 
+void fillarray(ifstream &infile, double *x, double *y, int size)
+{
+    char delimiter[] = ",";
+    string buffer;
+    for (int i = 0; i < size; i++)
+    {
+        infile >> buffer;
+        try
+        {
+        splitting(buffer, delimiter, &x[i], &y[i]);
+        }
+        catch (const invalid_argument)
+        {
+            i--;
+            continue;
+        }
+    }
+}
 int getlinenum(ifstream &infile)
 {
-    double linenum = 0;
+    int linenum = 0;
     string line;
+    char delimiter[] = ",";
     while (!infile.eof())
     {
         getline(infile, line);
-        ++linenum;
+        double x[sizeof(line)];
+        double y[sizeof(line)];
+        try
+        {
+        splitting(line, delimiter, x, y);
+        }
+        catch (const invalid_argument)
+        {
+            continue;
+        }
+        linenum++;
     }
     infile.clear();
     infile.seekg(0);
-    //Skip the "x,y" line
-    getline(infile,line);
-    //Accounting for x,y
-    return linenum - 1;
+    return linenum;
 }
 
-double sum(double arr[])
+double sum(double arr[], int size)
 {
     double sum = 0;
-    for (int i = 0; i < sizeof(*arr); i++)
+    for (int i = 0; i < size; i++)
     {
         sum += arr[i];
     }
@@ -100,17 +151,30 @@ double sum(double arr[])
 
 double mean(double arr[], int size)
 {
-    double aSum = sum(arr);
+    double aSum = sum(arr, size);
     return aSum / size;
 }
 
 double var(double arr[], int size){
-    // array is array of residuals, NOT of actual observations
-    double se[size]; // abb. for squared error, NOT standard error
+    double arrM = mean(arr, size);
+    double VAR = 0; // abb. for squared error, NOT standard error
     for(int i=0; i<size; i++){
-        se[i] = arr[i] * arr[i];
+        VAR += (arr[i] - arrM) * (arr[i] - arrM);
     }
-    return sum(se) / (size - 1);
+    return VAR / (size - 1);
+}
+
+double meanAbsoluteDeviation(double arr[], int size)
+{
+    double sum;
+    double aMean = mean(arr, size);
+    for (int i = 0; i < size; i++)
+    {
+        sum += fabs(arr[i] - aMean);
+    }
+    cout << sum / size << endl;
+
+    return sum / size;
 }
 
 void mode(double arr[], int size){
@@ -163,19 +227,19 @@ void mode(double arr[], int size){
     return; 
 }
 
-double cov(double resx[], double resy[], int size){
+double cov(double x[], double y[], int size){
     double resprod = 0;
+    double meanX = mean(x, size);
+    double meanY = mean(y, size);
     for(int i=0; i<size; i++){
-        resprod += resx[i] * resy[i]; 
+        resprod += (x[i] - meanX) * (y[i] - meanY); 
     }
     return resprod / (size - 1);
 }
 
-
-
 int main(int argc, char *argv[])
 {
-    //Checks if the program is executed in the correct format
+    // Checks if the program is executed in the correct format
     if (argc != 2)
     {
         cout << "Invalid amount of argument";
@@ -191,19 +255,21 @@ int main(int argc, char *argv[])
     }
 
     //Getting number of lines in file (to be used to create an array that fits the data)
-
     int size = getlinenum(infile);
     //Putting data set into arrays
     double x[size];
     double y[size];
+    fillarray(infile, x, y, size);
+
+    int arrSize = sizeof(x) / sizeof(y[0]);
+
+    // TESTING OUTPUT
+    cout << "size: " << arrSize << endl;
+    cout << "sum: " << sum(x, arrSize) << endl;
+    cout << "mean: " << mean(x, arrSize) << endl;
+    cout << "MAD: " << meanAbsoluteDeviation(x, arrSize) << endl;
     string buffer;
     char delimiter[] = ",";
-    for (int i = 0; i < size; i++)
-    {
-        infile >> buffer;
-        splitting(buffer, delimiter, &x[i], &y[i]);
-        // cout << y[i] << endl;
-    }
 
     // double a[] = {5, 10, 15};
     // int arrSize = sizeof(a) / sizeof(a[0]);
@@ -213,16 +279,24 @@ int main(int argc, char *argv[])
     double xMean = mean(x, size);
     double yMean = mean(y, size);
     double xResiduals[size], yResiduals[size];
-    for(int i=0; i<size; i++){
-        xResiduals[i] = x[i] - xMean;
-        yResiduals[i] = y[i] - yMean;
-    }
+    double covariance = cov(x, y, size);
+    double varX = var(x, size);
+    double varY = var(y, size);
+    double sdX = sqrt(varX);
+    double sdY = sqrt(varY);
+    double corr = covariance / (sdX * sdY);
+    cout << "covariance: " << covariance << endl; 
+    cout << "sd(X): " << sdX << endl;
+    cout << "sd(Y): " << sdY << endl;
+    cout << "pearson correlation: " << corr << endl;
     quickSort(x, 0, size);
     quickSort(y, 0, size);
 
-    int medianpos = ceil(size/2);
-
-    cout << "Median of x is " << x[medianpos] << endl << "Median of y is " << y[medianpos] << endl;
+    // TESTING SORTING
+    // print_array(x, arrSize);
+    // quickSort(x, 0, arrSize);
+    // print_array(x, arrSize);
+    // cout << "Median of x is " << x[medianpos] << endl << "Median of y is " << y[medianpos] << endl;
     // double x_mode = mode(x, size);
     // double y_mode = mode(y, size);
     // cout << "x has mode: " << x_mode << "\ny has mode: " << y_mode << endl; 
